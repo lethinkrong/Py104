1b34911b8e6a511fe16bfecff9a5b51512bd4ffd
puzzling <lethinkrong@gmail.com>
2017-08-06 09:19:05 +0800

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,9 @@
+#print("Hello World!")
+#print("Hello Again")
+#print('I "said" do not touch this.')
+#print("I like typing this.")
+#print('Yay! Printing.')
+#print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+print('\n')#多打印一行
+#注释
